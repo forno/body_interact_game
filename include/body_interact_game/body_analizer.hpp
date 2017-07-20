d2a75fca1e090fe2b2d598a018a81d8f52ebbc81
Doi Yusuke <o111027y@mail.kyutech.jp>
2017-07-21 08:33:46 +0900

    Refactor update function for body_analizer

--- include/body_interact_game/body_analizer.hpp
+++ include/body_interact_game/body_analizer.hpp
@@ -39,18 +39,7 @@ class body_analizer
 public:
   body_analizer(std::string root, std::size_t target_number);
 
-  void update()
-  {
-    const auto current_time {ros::Time{0}};
-    const auto base_pos {get_position(base_name_, current_time)};
-    const auto yaw_canceller {get_yaw_inverse_matrix(base_pos)};
-    head_ = yaw_canceller * base_pos.rotation() * Eigen::Vector3d::UnitY();
-    const auto base_vec {base_pos.translation()};
-    right_hand_ = yaw_canceller * (get_position(right_hand_name_, current_time).translation() - base_vec);
-    left_hand_ = yaw_canceller * (get_position(left_hand_name_, current_time).translation() - base_vec);
-    right_knee_ = yaw_canceller * (get_position(right_knee_name_, current_time).translation() - base_vec);
-    left_knee_ = yaw_canceller * (get_position(left_knee_name_, current_time).translation() - base_vec);
-  }
+  void update();
 
   Eigen::Vector3d get_head() const noexcept
   {
--- src/body_interact_game/body_analizer.cpp
+++ src/body_interact_game/body_analizer.cpp
@@ -10,3 +10,16 @@ body_analizer::body_analizer(std::string root, std::size_t target_number)
     left_knee_name_ {"left_knee_" + target_number_}
 {
 }
+
+void body_analizer::update()
+{
+  const auto current_time {ros::Time{0}};
+  const auto base_pos {get_position(base_name_, current_time)};
+  const auto yaw_canceller {get_yaw_inverse_matrix(base_pos)};
+  head_ = yaw_canceller * base_pos.rotation() * Eigen::Vector3d::UnitY();
+  const auto base_vec {base_pos.translation()};
+  right_hand_ = yaw_canceller * (get_position(right_hand_name_, current_time).translation() - base_vec);
+  left_hand_ = yaw_canceller * (get_position(left_hand_name_, current_time).translation() - base_vec);
+  right_knee_ = yaw_canceller * (get_position(right_knee_name_, current_time).translation() - base_vec);
+  left_knee_ = yaw_canceller * (get_position(left_knee_name_, current_time).translation() - base_vec);
+}
